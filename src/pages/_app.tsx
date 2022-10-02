e0c59025623194adabc7b65312bdef94d001ed7b
camboim557 <camboim557@gmail.com>
2022-10-02 09:14:17 -0300

    fetching products from graphql

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,18 @@
 import type { AppProps } from "next/app";
+import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
 import Home from "./home";
 
+const client = new ApolloClient({
+  uri: "http://localhost:3333/",
+  cache: new InMemoryCache(),
+});
+
 function MyApp({ pageProps }: AppProps) {
-  return <Home {...pageProps} />;
+  return (
+    <ApolloProvider client={client}>
+      <Home {...pageProps} />
+    </ApolloProvider>
+  );
 }
 
 export default MyApp;
--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,7 +1,71 @@
 import { NextPage } from "next";
+import { useQuery, gql } from "@apollo/client";
+import Image from "next/image";
+
+const GET_LOCATIONS = gql`
+  query (
+    $page: Int
+    $perPage: Int
+    $sortFilter: ProductFilter
+    $sortOrder: String
+    $sortField: String
+  ) {
+    allProducts(
+      page: 2
+      perPage: 20
+      sortOrder: $sortOrder
+      sortField: $sortField
+      filter: $sortFilter
+    ) {
+      id
+      name
+      imageUrl: image_url
+      priceInCents: price_in_cents
+      category
+    }
+    _allProductsMeta(perPage: $perPage, page: $page, filter: $sortFilter) {
+      count
+    }
+  }
+`;
 
 const Home: NextPage = () => {
-  return <> Homepage </>;
+  const { loading, error, data } = useQuery(GET_LOCATIONS);
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error :(</p>;
+
+  console.log(data?.allProducts);
+
+  //   return <>Home</>;
+
+  return data?.allProducts.map(
+    ({
+      id,
+      name,
+      priceInCents,
+      imageUrl,
+    }: {
+      id: string;
+      name: string;
+      priceInCents: number;
+      imageUrl: string;
+    }) => (
+      <div key={id}>
+        <h3>{name}</h3>
+        <Image
+          width="400"
+          height="250"
+          alt="location-reference"
+          src={`${imageUrl}`}
+        />
+        <br />
+        <b>About this location:</b>
+        <p>{priceInCents}</p>
+        <br />
+      </div>
+    )
+  );
 };
 
 export default Home;
